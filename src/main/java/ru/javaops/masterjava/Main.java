71f4290917c449c441fdf39c715bf85c93fa72cf
Semion Korobov <skorobov@tula.co>
2017-10-20 12:01:22 +0300

    HW01 concurrent multiply

--- src/main/java/ru/javaops/masterjava/Main.java
+++ src/main/java/ru/javaops/masterjava/Main.java
@@ -2,6 +2,10 @@ package ru.javaops.masterjava;
 
 import ru.javaops.masterjava.matrix.MatrixUtil;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * User: gkislin
  * Date: 05.08.2015
@@ -10,12 +14,19 @@ import ru.javaops.masterjava.matrix.MatrixUtil;
  * @link https://github.com/JavaOPs/topjava
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         int[][] matrixA = MatrixUtil.create(1000);
         int[][] matrixB = MatrixUtil.create(1000);
         long startTime = System.currentTimeMillis();
         int[][] resultMatrix = MatrixUtil.singleThreadMultiply(matrixA, matrixB);
         long endTime = System.currentTimeMillis();
         System.out.println("endTime-startTime = " + ((endTime - startTime) / 1000d) + " sec");
+
+        ExecutorService executors = Executors.newFixedThreadPool(4);
+        long startTime1 = System.currentTimeMillis();
+        int[][] resultMatrix2 = MatrixUtil.concurrentMultiply(matrixA, matrixB, executors);
+        long endTime1 = System.currentTimeMillis();
+        System.out.println("endTime-startTime = " + ((endTime1 - startTime1) / 1000d) + " sec");
+
     }
 }
--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,8 +1,14 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * gkislin
@@ -14,7 +20,31 @@ public class MatrixUtil {
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
-
+        CompletionService<Boolean> completionService = new ExecutorCompletionService<>(executor);
+        List<Future<Boolean>> futures = new LinkedList<>();
+        for (int j = 0; j < matrixSize; j++) {
+            final int jFinal = j;
+            Future<Boolean> future = completionService.submit(() -> {
+                int[] thatColumn = new int[matrixB.length];
+                for (int k = 0; k < matrixB.length; k++) {
+                    thatColumn[k] = matrixB[k][jFinal];
+                }
+                for (int i = 0; i < matrixSize; i++) {
+                    int thisRow[] = matrixA[i];
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += thisRow[k] * thatColumn[k];
+                    }
+                    matrixC[i][jFinal] = sum;
+                }
+                return true;
+            });
+            futures.add(future);
+        }
+        while (!futures.isEmpty()) {
+            Future<Boolean> future = completionService.poll(1, TimeUnit.SECONDS);
+            futures.remove(future);
+        }
         return matrixC;
     }
 
